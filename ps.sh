9be1b1cc3080066a00fadb2c52e144e27fb24b0f
xmdde <114699089+xmdde@users.noreply.github.com>
2022-11-23 22:32:05 +0100

    Update ps.sh

--- ps.sh
+++ ps.sh
@@ -11,8 +11,7 @@ then
 	read test <<< $(cat stat)
 	arr=( $test )
 	read files <<< $(sudo ls -l fd | wc -l)
-	echo ${arr[3]} "|" ${arr[0]} "|" ${arr[1]} "|" ${arr[2]} "|" ${arr[6]} "|" ${arr[23]} "|" 
-${arr[4]} "|" ${arr[5]} "|" $files >> /home/user/tmp.txt
+	echo ${arr[3]} "|" ${arr[0]} "|" ${arr[1]} "|" ${arr[2]} "|" ${arr[6]} "|" ${arr[23]} "|" ${arr[4]} "|" ${arr[5]} "|" $files >> /home/user/tmp.txt
 	cd ..
 fi
 done
